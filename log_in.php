ece45a51a6885906a060a4640d9ecec33b663b1a
Petr <86346448+Petruchio0@users.noreply.github.com>
2021-06-23 14:41:13 +0500

    Update log_in.php
    Обрабатываем введённые данные

--- log_in.php
+++ log_in.php
@@ -1,3 +1,63 @@
+<?php 
+	require "db/connectt.php";
+	$data = $_POST;
+	if (isset($data['in']))
+	{
+		if ($data['login'] == 'admin')
+		{
+
+			$eggor = array();
+			if ($data['passwd'] == 'kfreue34kv!mmcl')
+			{
+				$_SESSION['log_user'] = -1;	
+				$_SESSION['login'] = 'admin';
+  				header('location: index.php');
+			}
+			else 
+			{
+				$eggor[] = "Пароль не подходит";
+			}
+			if (!(empty($eggor)))
+			foreach ($eggor as $key => $value) {
+				echo $value . "</br>";
+		}
+		}
+		else
+		{
+		$eggor = array();
+		if (trim($data['login']) == '')
+		{
+			$eggor[] = "Введите логин";
+		} else
+		if (trim($data['passwd']) == '')
+		{
+			$eggor[] = "Введите пароль";
+		} 
+		$user = R::findOne('client', 'login = ?', array($data['login']));
+
+		if ($user)	
+		{
+			if (password_verify($data['passwd'], $user->password))
+			{
+				$_SESSION['log_user'] = $user->id;	
+				$_SESSION['login'] = $data['login'];
+  				header('location: index.php');
+  				echo "Поздравляем, вы есть " . $_SESSION['log_user'];
+  				exit;
+			}else $eggor[] = "Пароль не подходит";
+		}
+		else 
+		{
+			$eggor[] = "Пользователь не найден";
+		}
+		if (!(empty($eggor)))
+		foreach ($eggor as $key => $value) {
+			echo $value . "</br>";
+		}
+	}
+	}
+?>
+
 <title>Вход</title>
 <form action="log_in.php" method="POST">
 
